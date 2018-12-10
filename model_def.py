082e231a2ef282ba788f08a4e1e7a678b8a8b90a
khanhha <khanhhh89@gmail.com>
2018-12-10 16:21:10 +0100

    model on a single image type

--- model_def.py
+++ model_def.py
@@ -1,22 +1,82 @@
-from tensorflow.keras.layers  import Input, Conv2D, BatchNormalization, Dropout, Dense, MaxPooling2D, Flatten
-from tensorflow.keras import Model
+from keras.models import Model, Sequential
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Activation, BatchNormalization, Dropout
+from keras import optimizers
+from keras.regularizers import l2
 
-def create_model():
-    input = Input(shape=(27,27,3))
-    x = Conv2D(filters=16, kernel_size=3, strides=1, padding='same', activation='relu')(input)
-    x = Conv2D(filters=16, kernel_size=3, strides=1, padding='same', activation='relu')(x)
-    x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
 
-    x = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(x)
-    x = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(x)
+def create_sdnet_model():
+    # (3) Create a sequential model
+    model = Sequential()
 
-    x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
-    x = Flatten()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(25, activation='linear')(x)
+    # 1st Convolutional Layer
+    model.add(Conv2D(filters=96, input_shape=(224,224,3), kernel_size=(11,11), strides=(4,4), padding='valid'))
+    model.add(Activation('relu'))
+    # Pooling 
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation before passing it to the next layer
+    model.add(BatchNormalization())
 
-    model = Model(inputs = input, outputs = x)
-    model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
+    # 2nd Convolutional Layer
+    model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Pooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+    # 3rd Convolutional Layer
+    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+    # 4th Convolutional Layer
+    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+    # 5th Convolutional Layer
+    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(Activation('relu'))
+    # Pooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+    # Passing it to a dense layer
+    model.add(Flatten())
+    # 1st Dense Layer
+    model.add(Dense(4096, input_shape=(224*224*3,)))
+    model.add(Activation('relu'))
+    # Add Dropout to prevent overfitting
+    model.add(Dropout(0.4))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+    # 2nd Dense Layer
+    model.add(Dense(4096))
+    model.add(Activation('relu'))
+    # Add Dropout
+    model.add(Dropout(0.4))
+    # Batch Normalisation
+    model.add(BatchNormalization())
+
+#     # 3rd Dense Layer
+#     model.add(Dense(1000))
+#     model.add(Activation('relu'))
+#     # Add Dropout
+#     model.add(Dropout(0.4))
+#     # Batch Normalisation
+#     model.add(BatchNormalization())
+
+    # Output Layer
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    # (4) Compile 
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from pathlib import Path
 
 def extract_patch(img, h):
     patches = []
@@ -12,6 +13,75 @@ def extract_patch(img, h):
             patches.append(patch)
             patch_locs.append((i,j))
     return patches, patch_locs
+
+def extract_patch_1(img, h, offset, stride):
+    patches = []
+    patch_locs = []
+    d = h+offset
+    for i in range(d, img.shape[0]-d-1, stride):
+        for j in range(d, img.shape[1]-d-1, stride):
+            patch = img[i-h:i+h, j-h:j+h, :]
+            if patch.shape != (2*h, 2*h, 3):
+                print(patch.shape)
+            patches.append(patch)
+            patch_locs.append((i,j))
+    return patches, patch_locs
+
+def cut_image_into_patches(IN_DIR, OUT_DIR, img_target_size = (1024, 1024), patch_size = 224):
+    win_size = 224
+    h = int(win_size / 2)
+    for path in Path(IN_DIR).glob("*.*"):
+        img = cv.imread(str(path))
+        patches, patch_locs = extract_patch_1(img, h, offset=0, stride=win_size)
+        patches_1, patch_locs_1 = extract_patch_1(img, h, offset=h, stride=win_size)
+        patches = patches + patches_1
+        patch_locs = patch_locs + patch_locs_1
+        for patch, loc in zip(patches, patch_locs):
+            out_path = f'{OUT_DIR}/{path.stem}_{loc[0]}_{loc[1]}.png'
+            cv.imwrite(out_path, patch)
+
+def predict_image(model, img, crack_id = 0, debug_ax = None):
+    win_size = 224
+    h = int(win_size / 2)
+    patches, patch_locs = extract_patch_1(img, h, offset=0, stride=win_size)
+    patches_1, patch_locs_1 = extract_patch_1(img, h, offset=h, stride=win_size)
+    patches = patches+patches_1
+    patch_locs= patch_locs + patch_locs_1
+
+    #do it due to out of memory
+    pred_probs = []
+    for patch in patches:
+        input = np.expand_dims(patch, 0)
+        prob = model.predict(input)
+        pred_probs.append(prob.flatten())
+
+    pred_probs = np.array(pred_probs)
+    print(pred_probs)
+    print(pred_probs.shape)
+    preds = np.argmax(pred_probs, axis=1)
+    has_crack = False
+    if np.sum(preds == crack_id) > 0:
+        has_crack = True
+
+    if debug_ax is not None:
+        for i, pred in enumerate(preds):
+            if pred == crack_id:
+                loc = patch_locs[i]
+                pt0 = (loc[0]-h+5, loc[1]-h+5)
+                pt1 = (loc[0]+h-5, loc[1]+h-5)
+                if loc in patch_locs_1:
+                    cv.rectangle(img, pt0, pt1, color=(255, 255, 0), thickness=3)
+                else:
+                    cv.rectangle(img, pt0, pt1, color=(0, 255, 255), thickness=3)
+            debug_ax.imshow(img)
+
+    return has_crack
+
+def draw_crack_rectangle(img, crack_locs, axis, h):
+    for loc in crack_locs:
+        pt0 = (loc[0] - h + 5, loc[1] - h + 5)
+        pt1 = (loc[0] + h - 5, loc[1] + h - 5)
+        cv.rectangle(img, pt0, pt1, color=(255, 255, 0), thickness=3)
 
 def upsample_patch_predict(patch_pred, patch_locs, img_shape, h):
     img = np.zeros(img_shape[:2], dtype=np.float32)
